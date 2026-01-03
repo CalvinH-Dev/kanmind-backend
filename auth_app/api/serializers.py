1e5714eefc8c4e8e538ca6d1cc41501d990ada7d
CalvinH-Dev <hanisch.calvin@gmail.com>
2026-01-03 18:17:28 +0100

    add: added token to response at the registration

--- auth_app/api/serializers.py
+++ auth_app/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.authtoken.models import Token
 
 from auth_app.models import UserProfile
 
@@ -11,8 +12,19 @@ class RegistrationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ["email", "fullname", "password", "repeated_password"]
+        fields = [
+            "id",
+            "email",
+            "password",
+            "repeated_password",
+            "fullname",
+        ]
         extra_kwargs = {"password": {"write_only": True}}
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email already exists")
+        return value
 
     def validate(self, attrs):
         if attrs["password"] != attrs["repeated_password"]:
@@ -21,28 +33,33 @@ class RegistrationSerializer(serializers.ModelSerializer):
             )
         return attrs
 
-    def validate_email(self, value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("Email already exists")
-        return value
-
     def create(self, validated_data):
         pw = validated_data.pop("password")
         validated_data.pop("repeated_password")
         fullname = validated_data.pop("fullname")
         email = validated_data["email"]
 
-        account = User(email=email, username=email)
-        account.set_password(pw)
-        account.save()
+        user = User(email=email, username=email)
+        user.set_password(pw)
+        user.save()
+
+        UserProfile.objects.create(user=user, fullname=fullname)
 
         first_name, *last_parts = fullname.strip().split()
         last_name = " ".join(last_parts) if last_parts else ""
-        profile = UserProfile(user=account, fullname=fullname)
-        profile.save()
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
 
-        account.first_name = first_name
-        account.last_name = last_name
-        account.save()
+        return user
 
-        return account
+    def to_representation(self, instance):
+        profile = getattr(instance, "userprofile", None)
+        fullname = profile.fullname if profile else ""
+        token = Token.objects.create(user=instance)
+        return {
+            "token": token.key,
+            "fullname": fullname,
+            "email": instance.email,
+            "user_id": instance.id,
+        }
